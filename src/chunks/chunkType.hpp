a3c50af3f831814b0c51cbe967361b10bbe791e0
MeowyDev <meowy@meowerity.com>
2026-08-11 10:36:16 +0900

    added air and something i dont really remember uhhh

---
+++ src/chunks/chunkType.hpp
@@ -0,0 +1,13 @@
+// Made by Meowy
+// Using Clion (created with neovim tho)
+
+namespace nekocraft {
+
+    class Chunk {
+    public:
+
+    private:
+        int blocks[16][128][16] = {0};
+    };
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,56 @@
+#include "structs/Block.hpp"
+#include "structs/BlockID.hpp"
+#include "structs/Button.hpp"
 #include <iostream>
 #include <raylib.h>
 #include <vector>
-#include "structs/Button.hpp"
-#include "structs/Block.hpp"
 
-int main()
-{
+int main() {
 
-    InitWindow(1920,1080,"NekoCraft 3D");
+  InitWindow(1920, 1080, "NekoCraft 3D");
 
-    InitAudioDevice();
+  InitAudioDevice();
 
-    Sound GUISelect = LoadSound("assets/sound/select.mp3");
+  Sound GUISelect = LoadSound("assets/sound/select.mp3");
 
-    // Camera
+  // Camera
 
-    Camera3D cam;
+  Camera3D cam;
 
-    cam.fovy = 45.0f;
-    cam.position = {5.0f,3.0f,0.0f};
-    cam.projection = CAMERA_PERSPECTIVE;
-    cam.target = {0.0f,1.0f,0.0f};
-    cam.up = {0.0f,1.0f,0.0f};
+  cam.fovy = 45.0f;
+  cam.position = {5.0f, 3.0f, 0.0f};
+  cam.projection = CAMERA_PERSPECTIVE;
+  cam.target = {0.0f, 1.0f, 0.0f};
+  cam.up = {0.0f, 1.0f, 0.0f};
 
-    // Textures
+  // Textures
 
-    Texture2D grass = LoadTexture("assets/textures/grass.png");
-    SetTextureFilter(grass, TEXTURE_FILTER_POINT);
+  Texture2D grass = LoadTexture("assets/textures/grass.png");
+  SetTextureFilter(grass, TEXTURE_FILTER_POINT);
 
+  while (!WindowShouldClose()) {
 
+    int screenCenterX = GetScreenWidth() / 2;
+    int screenCenterY = GetScreenHeight() / 2;
+    Vector2 cursorPosScreen = GetMousePosition();
 
-    while (!WindowShouldClose()) {
+    UpdateCamera(&cam, CAMERA_FREE);
 
-        int screenCenterX = GetScreenWidth() / 2;
-        int screenCenterY = GetScreenHeight() / 2;
-        Vector2 cursorPosScreen = GetMousePosition();
+    BeginDrawing();
 
-        UpdateCamera(&cam, CAMERA_FREE);
+    BeginMode3D(cam);
 
-        BeginDrawing();
+    DrawGrid(10, 10);
 
-            BeginMode3D(cam);
+    EndMode3D();
 
-                DrawGrid(10,10);
+    EndDrawing();
 
-            EndMode3D();
+    ClearBackground(WHITE);
+  }
 
-        EndDrawing();
+  UnloadTexture(grass);
+  UnloadSound(GUISelect);
 
-        ClearBackground(WHITE);
-
-    }
-
-    UnloadTexture(grass);
-    UnloadSound(GUISelect);
-
-    CloseWindow();
+  CloseWindow();
 }
